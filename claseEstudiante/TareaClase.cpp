127f0bca396291d1fa53492f82a7c7617610c653
Dev3rik <117757693+Dev3rik@users.noreply.github.com>
2023-05-03 10:06:44 -0500

    Update TareaClase.cpp

--- claseEstudiante/TareaClase.cpp
+++ claseEstudiante/TareaClase.cpp
@@ -6,24 +6,38 @@
 using namespace std;
 void menu(){
     cout<<"-------------MENU DE ESTUDIANTES----------"<<endl;
-    cout<<"1. ingresar un numero al final"<<endl;
+    cout<<"1. ingresar un estudiante al final"<<endl;
     cout<<"2. ingresar en un posicion"<<endl;
     cout<<"3. eliminar un estudiante"<<endl;
-    cout<<"4. salir"<<endl;
+    cout<<"4. Mostrar listado"<<endl;
+    cout<<"5. salir"<<endl;
+    cout<<"elige una opcion:";
 }
 
 
 int main(){
     Students ArrayStudents;
     menu();
-    cout<<"elige una opcion:";int opc;cin >> opc;
-    while(opc!=4){
-    }
-    ArrayStudents.loginTheEndStudent();
-    ArrayStudents.loginTheEndStudent();
-    ArrayStudents.insertStudent();
-    ArrayStudents.removeStudent();
-    ArrayStudents.printStudent();
+    int opc;cin >> opc;
+    while(opc!=5){
 
+        if(opc==1){
+            ArrayStudents.loginTheEndStudent();
+        }
+        else if(opc==2){
+            ArrayStudents.insertStudent();
+        }
+        else if(opc==3){
+            ArrayStudents.removeStudent();
+        }
+        else if(opc==4){
+            ArrayStudents.printStudent();
+        }
+        else{
+            cout<<"no hay esa opcion"<<endl;
+        }
+        menu();
+        cin>>opc;
+    }
     return 0;
 }
